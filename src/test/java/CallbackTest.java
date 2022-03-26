d45623c035666d2a930aacb69f39279b9e592abf
Anna-64 <shurygina6410@gmail.com>
2022-03-26 12:42:50 +0400

    updated CallbackTest

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -16,13 +18,17 @@ public class CallbackTest {
 
     @BeforeAll
     public static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","./driver/win/chromedriver.exe");
-
+//        System.setProperty("webdriver.chrome.driver","./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void setUp(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
 
     }
 
